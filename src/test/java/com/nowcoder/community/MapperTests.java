cd03a7f26d4f997ed59e56a490369dbe8160ebe8
hupingsheng <hu942658636@163.com>
2023-04-30 18:45:42 +0800

    搭建Spring开发环境

---
+++ src/test/java/com/nowcoder/community/MapperTests.java
@@ -0,0 +1,55 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MapperTests {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelectUser(){
+        User user = userMapper.selectById(101);
+        System.out.println(user);
+
+        User user1 = userMapper.selectByName("liubei");
+        System.out.println(user1);
+    }
+
+    @Test
+    public void testInsertUser(){
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("123456");
+        user.setSalt("abc");
+        user.setEmail("test@qq.com");
+        user.setCreateTime(new Date());
+//        user.setActivationCode();
+        user.setHeaderUrl("http://www.nowcoder.com/101.png");
+
+        Integer rows = userMapper.insertUser(user);
+        System.out.println(rows);
+
+    }
+
+    @Test
+    public void update(){
+        Integer rows = userMapper.updateStatus(150, 1);
+        System.out.println(rows);
+
+    }
+
+
+}
